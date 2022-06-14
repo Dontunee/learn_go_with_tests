1e79f339f3055c147aea6e2940a4e9bf86f1edf1
Tunde <t_afolabi@yahoo.com>
2022-06-13 22:42:34 -0400

    include test for iteration using string builder

--- iterations.go
+++ iterations.go
@@ -1,1 +1,12 @@
 package code
+
+import "strings"
+
+func Repeat(input string, repeatCount int) string {
+	var sb strings.Builder
+	for i := 0; i < repeatCount; i++ {
+		sb.WriteString(input)
+	}
+
+	return sb.String()
+}
